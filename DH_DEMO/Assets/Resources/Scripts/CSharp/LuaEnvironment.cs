6140b7f4fe7afc920171c56346354261aa4ac763
SeanXmq <1667926277@qq.com>
2021-07-31 16:22:11 +0800

    xlua and base LuaBehaviour

---
+++ DH_DEMO/Assets/Resources/Scripts/CSharp/LuaEnvironment.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+public class LuaEnvironment 
+{
+    public static LuaEnvironment instance = new LuaEnvironment();
+    private LuaEnvironment(){}
+
+
+    //添加所需要的环境和访问方法
+
+    
+    private LuaEnv lua_env = new LuaEnv ();
+    public LuaEnv GetLuaEnv()
+    {
+        return lua_env;
+    }
+
+
+
+    
+
+
+
+    
+}
