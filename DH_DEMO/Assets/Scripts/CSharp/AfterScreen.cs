d19d879277834a6fafa9407e0787edc7fbfa1a73
MisThe <missthe0116@163.com>
2021-08-03 10:14:57 +0800

    添加后处理脚本

---
+++ DH_DEMO/Assets/Scripts/CSharp/AfterScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AfterScreen : MonoBehaviour
+{
+    public static Material base_material;
+    public static bool use_base = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera camera = this.GetComponent<Camera>();
+        if (camera != null)
+        {
+            camera.depthTextureMode = DepthTextureMode.DepthNormals;
+        }
+    }
+    private void OnRenderImage(RenderTexture src, RenderTexture dest) 
+    {
+        if (!use_base)
+        {
+            Graphics.Blit(src,dest);            
+        }
+        else
+        {
+            if (base_material != null)
+            {
+                Graphics.Blit(src,dest,base_material);
+            }
+        }
+    }
+}
