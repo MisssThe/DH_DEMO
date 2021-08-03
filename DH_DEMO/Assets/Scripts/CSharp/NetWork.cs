82e1e8f3a051a0412401f502dfbe5506cf8954a3
SeanXmq <1667926277@qq.com>
2021-08-03 14:09:56 +0800

    传输数据接口

--- DH_DEMO/Assets/Scripts/CSharp/NetWork.cs
+++ DH_DEMO/Assets/Scripts/CSharp/NetWork.cs
@@ -5,12 +5,15 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
-
+using System.IO;
+using Network;
+using Google.Protobuf;
 public class NetWork
 {
-    public static Socket GetConnect()
+    public Socket client;
+    NetWork()
     {
-        Socket client = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+        client = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
         EndPoint end_point = new IPEndPoint(IPAddress.Parse("10.0.9.66"),8800);
         try
         {
@@ -19,9 +22,10 @@ public class NetWork
         catch(SocketException e)
         {
             Debug.Log("连接服务器失败    " + e.Message);
-            return null;
+            
         }
         Debug.Log("链接成功");
+        ///////////////////////////////////////////////////////////到时候根据需求改收到服务器回复的处理
         ThreadStart receive = new ThreadStart(()=>{
             while(true)
             {
@@ -33,12 +37,81 @@ public class NetWork
         });
         Thread new_thread = new Thread(receive);
         new_thread.Start();
-        return client;
+    }
+    //序列化
+    public byte[] Serialize(IMessage message)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CodedOutputStream outputStream = new CodedOutputStream(memoryStream))
+                {
+                    message.WriteTo(outputStream);
+                    outputStream.Flush();
+        
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+
+    public void Deserialize(IMessage message, byte[] data)
+        {
+            using (CodedInputStream inputStream = new CodedInputStream(data))
+            {
+                message.MergeFrom(inputStream);
+            }
+        }
+    
+    //发送注册消息
+    public void SendRegister(string name, string password)
+    {
+        Register temp = new Register();
+        temp.Event.Event_ = "register";
+        temp.Name = name;
+        temp.Password = password;
+        byte[] data = Serialize(temp);
+        client.Send(data);
+    }
+    //发送登录消息
+    public void SendLogIn(string name, string password)
+    {
+        LogIn temp = new LogIn();
+        temp.Event.Event_ = "login";
+        temp.Name = name;
+        temp.Password = password;
+        byte[] data = Serialize(temp);
+        client.Send(data);
+    }
+    //发送聊天消息
+    public void SendTalk(string myName, string hisName, string msg)
+    {
+        Talk temp = new Talk();
+        temp.Event.Event_ = "talk";
+        temp.MyName = myName;
+        temp.HisName = hisName;
+        temp.Msg = msg;
+        byte[] data = Serialize(temp);
+        client.Send(data);
+    }
+    //发送战斗邀请
+    public void SendToFight(string myName, string hisName)
+    {
+        ToFight temp = new ToFight();
+        temp.Event.Event_ = "tofight";
+        temp.MyName = myName;
+        temp.HisName = hisName;
+        byte[] data = Serialize(temp);
+        client.Send(data);
+    }
+    //发送战斗信息
+    public void SendFight(string myName, string hisName, int carIndex)
+    {
+        Fight temp = new Fight();
+        temp.Event.Event_ = "fight";
+        temp.MyName = myName;
+        temp.HisName = hisName;
+        temp.CarIndex = carIndex;
+        byte[] data = Serialize(temp);
+        client.Send(data);
     }
 
-    public static void SendMessage(Socket client, byte[] message)
-    {
-        client.Send(message);
-        return;
-    }
 }
