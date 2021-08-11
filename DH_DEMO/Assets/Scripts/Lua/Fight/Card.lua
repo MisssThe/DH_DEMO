d9a296b746dc80eb46cb51772e75ccfbc64c7fe5
MisThe <missthe0116@163.com>
2021-08-11 15:41:13 +0800

    更新uimode ， bag

--- DH_DEMO/Assets/Scripts/Lua/Fight/Card.lua
+++ DH_DEMO/Assets/Scripts/Lua/Fight/Card.lua
@@ -1,14 +1,28 @@
-require("framework.SharedTools")
+-- require("framework.SharedTools")
 -- 卡牌基类
-Global.Card = {}
+-- Global.
+Card = {}
 Card.__index = Card
-function Card:New()
-    local temp
-    setmetatable(temp.Card)
+Card.type = nil
+-- Global.card_type = {"ATTACK","MAGIC","SUBSIDIARY"}
+-- card_type = CreateEnum(card_type)
+
+--------------------------------- 基础功能 -------------------------------
+function Card:New(type)
+    local temp = {}
+    setmetatable(temp,Card)
+    temp.type = type
     return temp
 end
 
--- 逻辑层
+-- 使用卡牌时产生的效果
+function Card:UseCard()
+    print("use base card")
+end
+
+--------------------------------- 逻辑层 ---------------------------------
+AttackCard = {}
 
 
--- 显示层
+
+--------------------------------- 显示层 ---------------------------------
