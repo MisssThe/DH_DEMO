b3946df4909794dd53f8f4a4afbfb78f9f1538e5
xiaodouxd <lianghengming@live.cn>
2021-08-12 13:22:02 +0800

    开始界面的UI微调

--- DH_DEMO/Assets/Scripts/CSharp/Dedicated/Overture/OvertureManager.cs
+++ DH_DEMO/Assets/Scripts/CSharp/Dedicated/Overture/OvertureManager.cs
@@ -13,7 +13,7 @@ public class OvertureManager : MonoBehaviour
     {
         if (atTheBeginning)
         {
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(5);
             atTheBeginning = false;
         }
         if (overtureManager.IsBuilding == false)
