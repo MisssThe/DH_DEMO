ea807c6f3cbbb6cfa31b06b745e5cfe1f402f617
xiaodouxd <lianghengming@live.cn>
2021-08-12 09:24:28 +0800

    事件系统在CSharp端的接口

--- DH_DEMO/Assets/Scripts/CSharp/GlobalMans/EventManagerCaller.cs
+++ DH_DEMO/Assets/Scripts/CSharp/GlobalMans/EventManagerCaller.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class EventManagerCaller : MonoBehaviour
 {
-    void Send(string eventName)
+    /// <summary>
+    /// <para> 发送事件 </para>
+    /// 事件内容的格式形如:
+    /// "1 \"eventType\" \"instName\" param1 param2 ..." 或 "0 \"eventType\" ...", 
+    /// 这里的第一个数字代表是否有instName, 后面则是要发送的具体事件内容, 各参数中间用空格隔开, 
+    /// </summary>
+    /// <param name="eventContent"> 事件内容 </param>
+    public void Send(string eventContent)
     {
-        EventManager.Instance.Send(eventName);
+        EventManager.Instance.Send(eventContent);
     }
 }
