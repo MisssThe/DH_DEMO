d756b22e0def2d135e033ca75f63e276aadfe27f
XiaoDouXd <1873316916@qq.com>
2021-08-13 20:21:15 +0800

    添加修改雾效果的动画

--- DH_DEMO/Assets/Scripts/Lua/Dedicated/Overture/OvertureManager.lua
+++ DH_DEMO/Assets/Scripts/Lua/Dedicated/Overture/OvertureManager.lua
@@ -24,6 +24,7 @@ end
 
 -- 卡片结束落下
 function overtureFunc.CardFallfin()
+    sea.gameObject:GetComponent(typeof(UE.MeshRenderer)).material:SetFloat("_FogAlpha", 0)
     cameraInLoading.gameObject:GetComponent(typeof(UE.Camera)).cullingMask = 33554432
 end
 
