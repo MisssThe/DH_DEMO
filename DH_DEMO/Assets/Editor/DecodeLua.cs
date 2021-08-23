3b229e22f445d5d32cc2413b28cb30b091ba1fc5
MisThe <missthe0116@163.com>
2021-08-23 16:14:27 +0800

    。

--- DH_DEMO/Assets/Editor/DecodeLua.cs
+++ DH_DEMO/Assets/Editor/DecodeLua.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEditor;
+using System.IO;
 public class DecodeLua : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [MenuItem("Tools/Decode Lua")]
+    public static void Decode()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        DirectoryInfo di = new DirectoryInfo("Assets/Scripts/Lua");
+        FileInfo[] fi = di.GetFiles("*", SearchOption.AllDirectories);
+        string new_str = null;
+        foreach (var item in fi)
+        {
+            int length = item.FullName.Length - 4;
+            new_str = item.FullName.Substring(length,4);
+            if (!new_str.Equals("meta"))
+            {
+                item.MoveTo(new_str + "bytes");
+                Debug.Log(item.FullName);
+            }
+        }
     }
 }
