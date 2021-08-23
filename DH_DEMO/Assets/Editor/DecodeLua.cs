8f719c7a08ffb20fc6632b7c2e672fd5b6b84d5c
MisThe <missthe0116@163.com>
2021-08-23 18:39:00 +0800

    。

--- DH_DEMO/Assets/Editor/DecodeLua.cs
+++ DH_DEMO/Assets/Editor/DecodeLua.cs
@@ -10,6 +10,19 @@ public class DecodeLua : MonoBehaviour
     {
         DirectoryInfo di = new DirectoryInfo("Assets/Scripts/Lua");
         FileInfo[] fi = di.GetFiles("*", SearchOption.AllDirectories);
+        // 保存lua asset文件内容
+        FileStream fs = new FileStream(@"Assets\AddressableAssetsData\AssetGroups\Lua Assets.asset",FileMode.Open, FileAccess.Read, FileShare.None);
+        StreamReader sr = new StreamReader(fs);
+        List<string> list = new List<string>();
+        string str = sr.ReadLine();
+        while (str != null)
+        {
+            list.Add(str);
+            str = sr.ReadLine();
+        }
+        sr.Close();
+
+
         string new_str = null;
         foreach (var item in fi)
         {
@@ -18,9 +31,15 @@ public class DecodeLua : MonoBehaviour
             if (!new_str.Equals("meta"))
             {
                 new_str = item.FullName.Substring(0,length + 1);
-                // item.MoveTo(new_str + "bytes");
-                Debug.Log(new_str + "bytes");
+                item.MoveTo(new_str + "bytes");
             }
         }
+
+        StreamWriter sw = new StreamWriter(@"Assets\AddressableAssetsData\AssetGroups\Lua Assets.asset",false);
+        foreach (var item in list)
+        {
+            sw.WriteLine(item);
+        }
+        sw.Close();
     }
 }
